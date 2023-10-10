6790adec919e62500c9d84532d7166aa2e6c07ac
Triston Stuart <triston.stuart@icloud.com>
2023-10-10 00:27:24 -0700

    Full version

--- index.js
+++ index.js
@@ -2,12 +2,34 @@ var loadingText = document.getElementById('loadingText');
 var errorText = document.getElementById('errorText');
 var username = document.getElementById('username');
 var password = document.getElementById('password');
+var downloadDiv = document.getElementById('downloads');
+
+function generateCSVlink(filename, data){
+  var fileOut = "Time, Speed (KMPH), Speed (MPH), Acceleration, Longitude, Latitude, SatelliteNum, Accuracy, Heading, Altitude\n";
+  var rawDataArr = data?.dataArr;
+  for (let item of rawDataArr){
+    // acceleration, accuracy, altitude, heading, latitude, longitude, satelliteNum, speed, time
+    fileOut += `${item?.time},${item?.speed},${Math.floor((item?.speed/1.609344) * 1000) / 1000},${item?.acceleration},${item?.longitude},${item?.latitude},${item?.satelliteNum},${item?.accuracy},${item?.heading},${item?.altitude}\n`
+  }
+
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(fileOut));
+  element.setAttribute('download', filename);
+  element.innerText = filename;
+  element.style.paddingRight = "10px";
+
+  downloadDiv.appendChild(element);
+}
 
 async function getData(){
   loadingText.hidden = false;
   errorText.hidden = true;
 
   var logindata = `userType=4&platform=5&platformId=${username.value}&password=${password.value}`;
+
+  while (downloadDiv.firstChild){
+    downloadDiv.removeChild(downloadDiv.lastChild);
+  }
 
   try{
     var response = await fetch("http://app.godragy.com/index.php/account/login", {
@@ -74,7 +96,8 @@ async function getData(){
               var result = entryData?.data?.carResults;
               var graphData = JSON.parse(result?.graph_data) || {};
               var dataInfo = JSON.parse(graphData?.dataInfo) || {};
-              console.log(dataInfo);
+
+              generateCSVlink(`dragy-${runId}.csv`, dataInfo);
             }else {
               errMsg += `Error [${entryData.err}]: ${entryData.errMsg}. `
             }
